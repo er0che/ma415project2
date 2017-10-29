7a50265b62942b83b3eab8d45695089756c80a3d
er0che <eroche@bu.edu>
2017-10-29 10:37:16 -0400

    push it real good

--- ma415project2.R
+++ ma415project2.R
@@ -1,18 +1,20 @@
-# brian's dataset
+# cape may dataset
 
 library(tidyverse)
 library(stringr)
 library(data.table)
+library(lubridate)
+library(dplyr)
 
-
-str1 <- "http://www.ndbc.noaa.gov/view_text_file.php?filename=mlrf1h"
+str1 <- "http://www.ndbc.noaa.gov/view_text_file.php?filename=44009h"
 str2 <- ".txt.gz&dir=data/historical/stdmet/"
 
-years <- c(1987:2016)
+
+years <- c(1984:2016)
 
 urls <- str_c(str1, years, str2, sep = "")
 
-filenames <- str_c("mr", years, sep = "")
+filenames <- str_c("cm", years, sep = "")
 
 N <- length(urls)
 
@@ -38,12 +40,22 @@ for (i in 1:N){
   file <- file %>% select(YYYY, MM, DD, hh, ATMP, WTMP)
   
   if(i == 1){
-    MR <- file
+    CM <- file
   }
   
   else{
-    MR <- rbind.data.frame(MR, file)
+    CM <- rbind.data.frame(CM, file)
   }
   
 }
 
+CM<-filter(CM, hh=="12", DD == '01' | DD =="15")
+
+CM$date <- ymd(paste(CM$YYYY, CM$MM, CM$DD))
+
+CM<-select(CM, date, ATMP, WTMP)
+
+CM$ATMP <- apply(CM[,2], MARGIN = 2, function(x){ifelse(x == 999.0, NA, x)})
+CM$ATMP <- apply(CM[,2], MARGIN = 2, function(x){ifelse(x == "999.0", NA, x)})
+CM$WTMP <- apply(CM[,3], MARGIN = 2, function(x){ifelse(x == 999.0, NA, x)})
+CM$WTMP <- apply(CM[,3], MARGIN = 2, function(x){ifelse(x == "999.0", NA, x)})
